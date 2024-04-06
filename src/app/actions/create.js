84a28b3edb8c54ef18daa55c71531ba497495a51
mathur619 <prakhar@fyno.io>
2024-04-06 13:08:32 +0530

    adding books in db redis

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,18 @@
-'use server'
+"use server";
 
-// import { client } from "@/lib/db"
-import { redirect } from 'next/navigation'
+import { client } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
+  const { title, rating, author, blurb } = Object.fromEntries(formData);
 
-  
+  const id = Math.floor(Math.random() * 10000);
+
+  await client.hSet(`books:${id}`, {
+    title,
+    rating,
+    author,
+    blurb,
+  });
+  redirect("/");
 }
