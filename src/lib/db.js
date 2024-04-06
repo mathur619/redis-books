fd006277cba3e8928f5998cbcc25d6abcf5ef0b7
mathur619 <prakhar@fyno.io>
2024-04-06 13:08:12 +0530

    assigning correct env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,14 @@
 import { createClient } from "redis";
 
 const client = createClient({
-  password: process.env.REDIS_PORT,
+  password: process.env.REDIS_PW,
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PW,
+    port: process.env.REDIS_PORT,
   },
 });
 
-client.on("error", (err) => console.log(err));
+client.on("error", (err) => console.log("🚀 ~ err:", err));
 
 if (!client.isOpen) {
   client.connect();
